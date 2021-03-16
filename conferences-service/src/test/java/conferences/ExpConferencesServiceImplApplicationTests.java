bca9d4751b8bd69a8271c422d1e668037fbb3883
VTrofymets_UDT <Vladyslav.Trofymets@aval.ua>
2021-03-16 14:20:11 +0200

    -Extract modules dependencies to parent

--- conferences-service/src/test/java/conferences/ExpConferencesServiceImplApplicationTests.java
+++ conferences-service/src/test/java/conferences/ExpConferencesServiceImplApplicationTests.java
@@ -1,13 +1,24 @@
 package conferences;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 class ExpConferencesServiceImplApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    private void createConference() {
+
     }
 
 }
